b0c037d1f451d08e7eeaedc26f8e62a64e576027
yashi3-scaler <yashi.tandon@scaler.com>
2022-02-08 12:25:51 +0530

    third commit

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -52,7 +52,7 @@ class TodoListsController < ApplicationController
     @todo_list.destroy
 
     respond_to do |format|
-      format.html { redirect_to todo_lists_url, notice: "Todo list was successfully destroyed." }
+      format.html { redirect_to root_url, notice: "Todo list was successfully destroyed." }
       format.json { head :no_content }
     end
   end
